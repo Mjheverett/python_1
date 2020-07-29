84ef8a4b44906f5790db7616a97628700fc2ed39
Matthew Everett <mjheverett@gmail.com>
2020-07-29 14:57:03 -0400

    completed factors

--- factors.py
+++ factors.py
@@ -1,1 +1,5 @@
-initialnum = int(input("Give me a number to factor." ))
+initialnum = int(input("Give me a number to factor: " ))
+for x in range (1, initialnum):
+    if (initialnum % x) == 0:
+        print(x)
+print(initialnum)
