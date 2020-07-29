d3f2fd93487589118ded39cff20c505ab1296e7b
Matthew Everett <mjheverett@gmail.com>
2020-07-29 14:43:50 -0400

    completed box

--- box.py
+++ box.py
@@ -1,2 +1,8 @@
 width = int(input("Width? "))
 height = int(input("Height? "))
+
+for y in range (1, height + 1):
+    if y == 1 or y == height:
+        print("*" * width)
+    elif 1 < y < height:
+        print("*" + (" " * (width - 2)) + "*")
