fe9c1134865797f5a8e1d82e40124eb70fda4868
Matthew Everett <mjheverett@gmail.com>
2020-07-29 12:22:27 -0400

    clean up code

--- guess_the_number.py
+++ guess_the_number.py
@@ -2,24 +2,19 @@ import random
 playagain = "Y"
 while playagain == "Y":
     secret_number = random.randint(1, 10)
-    guessesleft = 5
     print("I am thinking of a number between 1 and 10.")
-    print("You have %d guesses left." % guessesleft)
-    guess = int(input("What's the number? "))
-    while (guessesleft > 0) & (guess != secret_number):
-        if guessesleft == 0:
-            print("You have no guesses left.")
-        else:
-            if guess == secret_number:
-                print("Yes! You win!")
-            elif guess < secret_number:
-                print("%d is too low." % guess)
-                guessesleft -= 1
-            elif guess > secret_number:
-                print("%d is too high." % guess)
-                guessesleft -= 1
-        print("You have %d guesses left." % guessesleft)
-        guess = int(input("What's the number? "))
+    guess = int(input("What's the number? ")) 
+    if guess == secret_number:
+        print("Yes! You win!")
+    elif guess < secret_number:
+        print("%d is too low." % guess)
+        guessesleft -= 1
+    elif guess > secret_number:
+        print("%d is too high." % guess)
+        guessesleft -= 1    
+    else:
+        print("You have no guesses left." % guessesleft)
         
+
     playagain = input("Would you like to play again? (Y or N)")
 print("Bye.")
--- multiplication_table.py
+++ multiplication_table.py
@@ -2,5 +2,4 @@ m = 1
 n = 1
 for m in range(1, 11):
     for n in range(1,11):
-        x = m * n
-        print("%d X %d = %d" % (m,n,x))
+        print("%d X %d = %d" % (m, n, m*n))
