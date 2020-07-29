0d3b04dd821421411098674e68106f5da75e178e
Matthew Everett <mjheverett@gmail.com>
2020-07-29 12:39:19 -0400

    working on guess_the_number

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,6 +1,6 @@
 import random
-playagain = "Y"
-while playagain == "Y":
+
+def guess_the_number():
     secret_number = random.randint(1, 10)
     print("I am thinking of a number between 1 and 10.")
     guess = int(input("What's the number? ")) 
@@ -8,13 +8,13 @@ while playagain == "Y":
         print("Yes! You win!")
     elif guess < secret_number:
         print("%d is too low." % guess)
-        guessesleft -= 1
     elif guess > secret_number:
         print("%d is too high." % guess)
-        guessesleft -= 1    
-    else:
-        print("You have no guesses left." % guessesleft)
-        
 
+playagain = "Y"
+
+while playagain == "Y":
+    guess_the_number()
     playagain = input("Would you like to play again? (Y or N)")
+
 print("Bye.")
--- loops.py
+++ loops.py
@@ -1,6 +1,7 @@
 title = "Electric Boogaloo"
-STOP = 10
+# STOP = 10
 counter = 0
-while counter < STOP:
+
+while counter < len(title):
     print(title[counter])
     counter += 1
